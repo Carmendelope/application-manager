07d1ab52ff9484bd8c3b25e5b0ac759d1f11015e
Carmen de Lope <cdelope@daisho.group>
2019-04-25 08:50:26 +0200

    NP-1112. Separate the validation in different functions

--- internal/pkg/entities/parameters.go
+++ internal/pkg/entities/parameters.go
@@ -356,11 +356,69 @@ func CreateParametrizedDescriptor (descriptor *grpc_application_go.AppDescriptor
 		return parametrized, nil
 }
 
+// validateParamName checks the param name does not start whit "NALEJ_"
+func validateParamName (name string) derrors.Error {
+	if strings.HasPrefix(name,nalejPrefix) {
+		return derrors.NewInvalidArgumentError(invalidParamName).WithParams(name)
+	}
+	return nil
+}
+
+// validateParamPath validates that the path allows access to a defined field
+func validateParamPath (jsonDescriptor string, param *grpc_application_go.AppParameter) (gjson.Result, derrors.Error) {
+
+	path := param.Path
+	field := gjson.Get(jsonDescriptor, path)
+	if ! field.Exists() {
+		return field, derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
+	}
+
+	return field, nil
+}
+// validateParamType validates that the defined type corresponds to the value by default
+func validateParamType (field gjson.Result, param *grpc_application_go.AppParameter) derrors.Error {
+
+	fieldType := field.Type
+	// validate Type
+	switch param.Type {
+	case grpc_application_go.ParamDataType_BOOLEAN:
+		if fieldType != gjson.False && fieldType != gjson.True {
+			return derrors.NewInvalidArgumentError(invalidParamType).WithParams(param.Name)
+		}
+	case grpc_application_go.ParamDataType_INTEGER:
+		if fieldType != gjson.Number {
+			return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
+		}
+	case grpc_application_go.ParamDataType_FLOAT:
+		if fieldType != gjson.Number {
+			return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
+		}
+	case grpc_application_go.ParamDataType_ENUM:
+		if fieldType != gjson.String {
+			return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
+		}
+	case grpc_application_go.ParamDataType_STRING:
+		if fieldType != gjson.String {
+			return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
+		}
+	case grpc_application_go.ParamDataType_PASSWORD:
+		if fieldType != gjson.String {
+			return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
+		}
+	}
+
+	return nil
+}
+
+// validateAllowedParameter validates that the path is form a allowed field
+func validateAllowedParameter (param *grpc_application_go.AppParameter) derrors.Error {
+	return nil
+}
 
 // ValidateDescriptorParameters validates the parameter has correct format, the path is allowed (field can be updated) and the name is correct (no starts with NALEJ_)
 func ValidateDescriptorParameters (descriptor *grpc_application_go.AddAppDescriptorRequest) derrors.Error {
 
-	// we need to convert the parametrized descriptor to json to apply changes
+	// we need to convert the parametrized descriptor to json get values
 	newDescriptor, err:= json.Marshal(descriptor)
 	if err != nil {
 		return conversions.ToDerror(err)
@@ -376,55 +434,35 @@ func ValidateDescriptorParameters (descriptor *grpc_application_go.AddAppDescrip
 
 		// validate name
 		name:= strings.ToUpper(strings.TrimLeft(param.Name, "") )
-		if strings.HasPrefix(name,nalejPrefix) {
-			return derrors.NewInvalidArgumentError(invalidParamName).WithParams(param.Name)
+		valErr := validateParamName(name)
+		if valErr != nil {
+			return valErr
 		}
+		// check the name is not defined twice
 		exists,_ := paramNames[name]
 		if exists {
 			return derrors.NewInvalidArgumentError(paramDefinedTwice).WithParams(param.Name)
 		}
 		paramNames[name] = true
 
-		// validate path
-		path := param.Path
-		field := gjson.Get(jsonDescriptor, path)
-		if ! field.Exists() {
-			return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
+		// validate param path and type
+		field, valErr := validateParamPath(jsonDescriptor, param)
+		if valErr != nil {
+			return valErr
 		}
 
-		fieldType := field.Type
-		// validate Type
-		switch param.Type {
-		case grpc_application_go.ParamDataType_BOOLEAN:
-			if fieldType != gjson.False && fieldType != gjson.True {
-				return derrors.NewInvalidArgumentError(invalidParamType).WithParams(param.Name)
-			}
-		case grpc_application_go.ParamDataType_INTEGER:
-			if fieldType != gjson.Number {
-				return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
-			}
-		case grpc_application_go.ParamDataType_FLOAT:
-			if fieldType != gjson.Number {
-				return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
-			}
-		case grpc_application_go.ParamDataType_ENUM:
-			if fieldType != gjson.String {
-				return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
-			}
-		case grpc_application_go.ParamDataType_STRING:
-			if fieldType != gjson.String {
-				return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
-			}
-		case grpc_application_go.ParamDataType_PASSWORD:
-			if fieldType != gjson.String {
-				return derrors.NewInvalidArgumentError(invalidParamPath).WithParams(param.Name)
-			}
+		// validate type
+		valErr = validateParamType(field, param)
+		if valErr != nil {
+			return valErr
 		}
 		// default_value
 		param.DefaultValue = field.String()
 
-		// TODO: check that the parameters modify allowed fields
-
+		valErr = validateAllowedParameter(param)
+		if valErr != nil {
+			return valErr
+		}
 
 	}
 	return nil
