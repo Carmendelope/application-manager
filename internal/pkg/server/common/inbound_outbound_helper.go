21679b01d8aa1fee35c30db6c66311c87c867e99
Carmen de Lope <cdelope@daisho.group>
2019-09-16 12:33:37 +0200

    NP-1958 comments added

--- internal/pkg/server/common/inbound_outbound_helper.go
+++ internal/pkg/server/common/inbound_outbound_helper.go
@@ -7,7 +7,7 @@ import "github.com/nalej/grpc-application-go"
 
 // this file contains some util functions to check inbounds and outbounds of an organization
 
-
+// getInstance returns the instance whose name matches the instanceId if it exists in the list of instances
 func getInstance (list *grpc_application_go.AppInstanceList, appInstanceID string) *grpc_application_go.AppInstance{
 	for _, instance := range list.Instances {
 		if instance.AppInstanceId == appInstanceID {
@@ -17,6 +17,7 @@ func getInstance (list *grpc_application_go.AppInstanceList, appInstanceID strin
 	return nil
 }
 
+// InstanceExists check if an instance exist in the list
 func InstanceExists(list *grpc_application_go.AppInstanceList, appInstanceID string) bool {
 
 	instance := getInstance (list, appInstanceID)
@@ -28,6 +29,7 @@ func InstanceExists(list *grpc_application_go.AppInstanceList, appInstanceID str
 	return false
 }
 
+// InboundExists check if the exists an instance with appInstanceID identifier and it has an inbound with the inboundName defined
 func InboundExists (list *grpc_application_go.AppInstanceList, appInstanceID string, inboundName string) bool {
 
 	instance := getInstance (list, appInstanceID)
@@ -46,6 +48,7 @@ func InboundExists (list *grpc_application_go.AppInstanceList, appInstanceID str
 	return false
 }
 
+// OutboundExists check if the exists an instance with appInstanceID identifier and it has an outbound with the inboundName defined
 func OutboundExists (list *grpc_application_go.AppInstanceList, appInstanceID string, outboundName string) bool {
 
 	instance := getInstance (list, appInstanceID)
